1a9ea7b061b26262dc8ca518613d33e68b053810
NamAnh <nam.work1909@gmail.com>
2022-07-19 08:25:01 +0700

    update profile

--- src/resident/screens/profile/changePass.js
+++ src/resident/screens/profile/changePass.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Toast, { DURATION } from 'react-native-easy-toast';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Toast, { DURATION } from "react-native-easy-toast";
 import {
   Alert,
   ScrollView,
@@ -14,61 +14,61 @@ import {
   Modal,
   TouchableWithoutFeedback,
   FlatList,
-  TextInput
-} from 'react-native';
+  TextInput,
+} from "react-native";
 
-import Color from '../../theme/colors';
-import { flag_en, flag_vn } from '../../theme/images';
-import colors from '../../theme/colors';
-import LanguageItem from '../../components/profile/LanguageItem';
-import { changePass } from '../../../actions/auth';
-import { MyIcon } from '../../theme/icons';
-import Strings from '../../utils/languages';
-import ImageProgress from '../../components/common/ImageProgress';
-import ErrorContent from '../../components/common/ErrorContent'
-import CheckBox from '../../components/common/CheckBox';
-import PrimaryButton from '../../components/common/PrimaryButton';
-import fontsize from '../../theme/fontsize';
-import NavBar from '../../components/common/NavBar';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Screen } from '../../utils/device';
-import { getProfile } from '../../actions/auth';
+import Color from "../../theme/colors";
+import { flag_en, flag_vn } from "../../theme/images";
+import colors from "../../theme/colors";
+import LanguageItem from "../../components/profile/LanguageItem";
+import { changePass } from "../../../actions/auth";
+import { MyIcon } from "../../theme/icons";
+import Strings from "../../utils/languages";
+import ImageProgress from "../../components/common/ImageProgress";
+import ErrorContent from "../../components/common/ErrorContent";
+import CheckBox from "../../components/common/CheckBox";
+import PrimaryButton from "../../components/common/PrimaryButton";
+import fontsize from "../../theme/fontsize";
+import NavBar from "../../components/common/NavBar";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { Screen } from "../../utils/device";
+import { getProfile } from "../../actions/auth";
 class ChangePassScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
-    header: null
-  })
+    header: null,
+  });
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      phoneNumber: '',
-      password: '',
-      passwordRetype: ''
-    }
+      phoneNumber: "",
+      password: "",
+      passwordRetype: "",
+    };
   }
   componentDidMount() {
-    this.props.navigation.dispatch({ type: 'CHANGEPASS_START' });
+    this.props.navigation.dispatch({ type: "CHANGEPASS_START" });
   }
   async componentWillReceiveProps(nextProps) {
     // console.log(this.props)
     // console.log(nextProps)
     if (nextProps.error && nextProps.error !== this.props.error) {
       if (nextProps.error.hasError) {
-        let message = '';
+        let message = "";
         switch (nextProps.error.message) {
-          case 'CODE_OTP_INVALID':
+          case "CODE_OTP_INVALID":
             message = Strings.message.CODE_OTP_INVALID;
             break;
-          case 'DATA_NOT_FOUND':
+          case "DATA_NOT_FOUND":
             message = Strings.message.DATA_NOT_FOUND;
             break;
-          case 'INVALID_ACCESS':
+          case "INVALID_ACCESS":
             message = Strings.message.INVALID_ACCESS;
             break;
-          case 'RESIDENTS_NOT_IN_THE_BUILDING':
+          case "RESIDENTS_NOT_IN_THE_BUILDING":
             message = Strings.message.RESIDENTS_NOT_IN_THE_BUILDING;
             break;
-          case 'PASS_INVALID':
+          case "PASS_INVALID":
             message = Strings.message.PASS_INVALID;
             break;
 
@@ -79,49 +79,71 @@ class ChangePassScreen extends Component {
         Alert.alert(Strings.message.alert, message);
       }
     }
-    if (!nextProps.isLoading && nextProps.content == 'CHANGEPASS_SUCCESS' && nextProps.content !== this.props.content) {
-      await Alert.alert(Strings.message.alert, 'Thành công');
-      await this.props.getProfile({ type: 're', langId: this.props.langId });
-      await this.props.navigation.goBack()
+    if (
+      !nextProps.isLoading &&
+      nextProps.content == "CHANGEPASS_SUCCESS" &&
+      nextProps.content !== this.props.content
+    ) {
+      await Alert.alert(Strings.message.alert, "Thành công");
+      await this.props.getProfile({ type: "re", langId: this.props.langId });
+      await this.props.navigation.goBack();
     }
   }
 
   _onChange = () => {
-    const { phoneNumber, password, passwordRetype } = this.state
-    console.log('this.state', phoneNumber)
-    if (phoneNumber.length == 0 || password.length == 0 || passwordRetype.length == 0) {
+    const { phoneNumber, password, passwordRetype } = this.state;
+    console.log("this.state", phoneNumber);
+    if (
+      phoneNumber.length == 0 ||
+      password.length == 0 ||
+      passwordRetype.length == 0
+    ) {
       Alert.alert(Strings.tabbar.notification, Strings.login.notify1);
     } else if (password !== passwordRetype) {
       Alert.alert(Strings.tabbar.notification, Strings.login.notify2);
     } else {
-      this.props.changePass({ phoneNumber, password })
+      this.props.changePass({ phoneNumber, password });
     }
-  }
+  };
 
   render() {
     const { phoneNumber, password, passwordRetype } = this.state;
     return (
-      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={{ flex: 1, backgroundColor: "#fff" }}>
         <NavBar
-          
-          leftButton={this.props.user && this.props.user.isChangePass && <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{ padding: 10 }}><MyIcon name="arrow" color="#fff" size={20} /></TouchableOpacity>}
-          body={<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text style={{
-            fontFamily: "Inter-Bold",
-            fontSize: 20,
-            fontWeight: "bold",
-            fontStyle: "normal",
-            lineHeight: 24,
-            letterSpacing: 0,
-            textAlign: "center",
-            color: "#ffffff"
-          }}>{Strings.login.changePass}</Text></View>}
-          rightView={
-            <View style={{ padding: 10 }}>
-              <MyIcon
-                name="arrow"
-                size={22}
-                color={colors.appTheme}
-              />
+          leftButton={
+            this.props.user &&
+            this.props.user.isChangePass && (
+              <TouchableOpacity
+                onPress={() => this.props.navigation.goBack()}
+                style={{ padding: 10 }}
+              >
+                <MyIcon name="arrow" color="black" size={20} />
+              </TouchableOpacity>
+            )
+          }
+          body={
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: "Inter-Bold",
+                  fontSize: 20,
+                  fontWeight: "bold",
+                  fontStyle: "normal",
+                  lineHeight: 24,
+                  letterSpacing: 0,
+                  textAlign: "center",
+                  color: "black",
+                }}
+              >
+                {Strings.login.changePass}
+              </Text>
             </View>
           }
         />
@@ -129,9 +151,9 @@ class ChangePassScreen extends Component {
         <View
           style={{
             flex: 1,
-            alignItems: 'center',
+            alignItems: "center",
             //justifyContent: 'center',
-            padding: 20
+            padding: 20,
           }}
         >
           {/* <MyIcon
@@ -142,11 +164,11 @@ class ChangePassScreen extends Component {
           /> */}
           <View
             style={{
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: "row",
+              alignItems: "center",
               borderBottomWidth: 0.5,
               borderColor: "#cccccc",
-              marginBottom: Platform.OS === 'ios' ? 20 : 10
+              marginBottom: Platform.OS === "ios" ? 20 : 10,
             }}
           >
             <TextInput
@@ -158,7 +180,7 @@ class ChangePassScreen extends Component {
                 fontStyle: "normal",
                 letterSpacing: 0,
                 textAlign: "left",
-                color: "#262626"
+                color: "#262626",
               }}
               autoCapitalize="none"
               autoFocus={false}
@@ -170,18 +192,17 @@ class ChangePassScreen extends Component {
               placeholder={Strings.login.phone}
               placeholderTextColor="#666666"
               underlineColorAndroid="transparent"
-            //onSubmitEditing={onSubmitEditing}
+              //onSubmitEditing={onSubmitEditing}
             />
             <MyIcon name="phone" size={18} color="#666666" />
           </View>
           <View
-
             style={{
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: "row",
+              alignItems: "center",
               borderBottomWidth: 0.5,
               borderColor: "#cccccc",
-              marginBottom: Platform.OS === 'ios' ? 20 : 10
+              marginBottom: Platform.OS === "ios" ? 20 : 10,
             }}
           >
             <TextInput
@@ -193,7 +214,7 @@ class ChangePassScreen extends Component {
                 fontStyle: "normal",
                 letterSpacing: 0,
                 textAlign: "left",
-                color: "#262626"
+                color: "#262626",
               }}
               autoCapitalize="none"
               autoFocus={false}
@@ -206,18 +227,17 @@ class ChangePassScreen extends Component {
               placeholderTextColor="#666666"
               secureTextEntry={true}
               underlineColorAndroid="transparent"
-            //onSubmitEditing={onSubmitEditing}
+              //onSubmitEditing={onSubmitEditing}
             />
             <Icon name="lock" size={18} color="#666666" />
           </View>
           <View
-
             style={{
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: "row",
+              alignItems: "center",
               borderBottomWidth: 0.5,
               borderColor: "#cccccc",
-              marginBottom: Platform.OS === 'ios' ? 20 : 10
+              marginBottom: Platform.OS === "ios" ? 20 : 10,
             }}
           >
             <TextInput
@@ -237,12 +257,14 @@ class ChangePassScreen extends Component {
               autoCorrect={false}
               //maxLength={15}
               value={passwordRetype}
-              onChangeText={(passwordRetype) => this.setState({ passwordRetype: passwordRetype })}
+              onChangeText={(passwordRetype) =>
+                this.setState({ passwordRetype: passwordRetype })
+              }
               placeholder={Strings.login.passwordRetypeNew}
               placeholderTextColor="#666666"
               secureTextEntry={true}
               underlineColorAndroid="transparent"
-            //onSubmitEditing={onSubmitEditing}
+              //onSubmitEditing={onSubmitEditing}
             />
             <Icon name="lock" size={18} color="#666666" />
           </View>
@@ -253,55 +275,71 @@ class ChangePassScreen extends Component {
               width: Screen.width * 0.8,
               height: 40,
               backgroundColor: colors.appTheme,
-              justifyContent: 'center'
+              justifyContent: "center",
             }}
             onPress={() => this._onChange()}
           >
-            <Text style={{
-              fontFamily: "Inter-SemiBold",
-              fontSize: 18,
-              fontWeight: "600",
-              fontStyle: "normal",
-              lineHeight: 28,
-              letterSpacing: 0,
-              textAlign: "center",
-              color: "#ffffff"
-            }}>{Strings.login.button1}</Text>
+            <Text
+              style={{
+                fontFamily: "Inter-SemiBold",
+                fontSize: 18,
+                fontWeight: "600",
+                fontStyle: "normal",
+                lineHeight: 28,
+                letterSpacing: 0,
+                textAlign: "center",
+                color: "#ffffff",
+              }}
+            >
+              {Strings.login.button1}
+            </Text>
           </TouchableOpacity>
-          {this.props.user && !this.props.user.isChangePass && <Text style={{color: 'red', fontStyle: 'italic', marginHorizontal: 20, marginVertical: 40}}>
-            Đây là yêu cầu bắt buộc từ Ban quản lý tòa nhà. Vui lòng đổi mật khẩu để sử dụng các tiện ích khác!
-          </Text>}
+          {this.props.user && !this.props.user.isChangePass && (
+            <Text
+              style={{
+                color: "red",
+                fontStyle: "italic",
+                marginHorizontal: 20,
+                marginVertical: 40,
+              }}
+            >
+              Đây là yêu cầu bắt buộc từ Ban quản lý tòa nhà. Vui lòng đổi mật
+              khẩu để sử dụng các tiện ích khác!
+            </Text>
+          )}
         </View>
         {this.props.isLoading && <ActivityIndicator />}
       </View>
-    )
+    );
   }
-
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   menuContent: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingVertical: 10
-  }
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+  },
 });
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   user: state.auth.user,
   language: state.app.language,
   isLoading: state.auth.isLoading,
   error: state.auth.error,
   content: state.auth.content,
-  langId: state.app.language == 'vi' ? 1 : 2,
+  langId: state.app.language == "vi" ? 1 : 2,
 });
 const mapActionToState = {
   changePass,
-  getProfile
+  getProfile,
 };
 
-export default connect(mapStateToProps, mapActionToState)(ChangePassScreen);
+export default connect(
+  mapStateToProps,
+  mapActionToState
+)(ChangePassScreen);
--- src/screens/profile/Setting.js
+++ src/screens/profile/Setting.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import {
   Alert,
   ScrollView,
@@ -11,33 +11,31 @@ import {
   StyleSheet,
   ActivityIndicator,
   Modal,
-  TouchableWithoutFeedback
-} from 'react-native';
+  TouchableWithoutFeedback,
+} from "react-native";
 
-import fontsize from '../../theme/fontsize';
-import Color from '../../theme/colors';
-import { flag_en, flag_vn } from '../../theme/images';
-import colors from '../../theme/colors';
-import LanguageItem from '../../components/profile/LanguageItem';
-import { resetStateByKey } from '../../actions/app';
-import { MyIcon } from '../../theme/icons';
-import Strings from '../../utils/languages';
-import NavBar from '../../resident/components/common/NavBar';
-
-
+import fontsize from "../../theme/fontsize";
+import Color from "../../theme/colors";
+import { flag_en, flag_vn } from "../../theme/images";
+import colors from "../../theme/colors";
+import LanguageItem from "../../components/profile/LanguageItem";
+import { resetStateByKey } from "../../actions/app";
+import { MyIcon } from "../../theme/icons";
+import Strings from "../../utils/languages";
+import NavBar from "../../resident/components/common/NavBar";
 
 class SettingScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     headerTitle: Strings.setting.language.toLocaleUpperCase(),
     headerBackTitle: null,
-    headerTintColor: '#fff',
+    headerTintColor: "#fff",
     headerStyle: {
       backgroundColor: colors.appTheme,
       elevation: 0,
-      borderBottomColor: 'transparent',
-      borderBottomWidth: 0
+      borderBottomColor: "transparent",
+      borderBottomWidth: 0,
     },
-  })
+  });
 
   constructor(props) {
     super(props);
@@ -52,93 +50,92 @@ class SettingScreen extends Component {
   }
 
   onValueChange(key, value) {
-    this.props.resetStateByKey({ key: value })
+    this.props.resetStateByKey({ key: value });
   }
 
-
-
   render() {
-    const languageValue = this.props.language == 'vi' ? 0 : 1
+    const languageValue = this.props.language == "vi" ? 0 : 1;
     const leftButton = (
       <TouchableOpacity
         style={{ padding: 10 }}
         onPress={() => this.props.navigation.goBack(null)}
       >
-        <MyIcon
-          name="arrow"
-          size={22}
-          color="#fff"
-        />
+        <MyIcon name="arrow" size={22} color="black" />
       </TouchableOpacity>
-    )
+    );
     return (
       <View style={styles.container}>
-        <NavBar leftButton={leftButton}
-          body={<Text style={{
-            alignSelf: 'center', fontFamily: "Inter-Bold",
-            fontSize: 20,
-            fontWeight: "bold",
-            fontStyle: "normal",
-            letterSpacing: 0,
-            textAlign: "center",
-            color: "#ffffff"
-          }}>{Strings.profile.setting}</Text>}
-          rightView={
-            <View style={{ padding: 10 }}>
-              <MyIcon
-                name="arrow"
-                size={22}
-                color={colors.appTheme}
-              />
-            </View>
+        <NavBar
+          leftButton={leftButton}
+          body={
+            <Text
+              style={{
+                alignSelf: "center",
+                fontFamily: "Inter-Bold",
+                fontSize: 20,
+                fontWeight: "bold",
+                fontStyle: "normal",
+                letterSpacing: 0,
+                textAlign: "center",
+                color: "black",
+              }}
+            >
+              {Strings.profile.language}
+            </Text>
           }
         />
-        <ScrollView style={{ borderTopRightRadius: 20, marginTop: -20 }}>
-
-          <Text style={{ marginLeft: 10, marginVertical: 10, fontFamily: "Inter-Bold",
-                    fontSize: 20,
-                    fontWeight: "bold",
-                    fontStyle: "normal",
-                    letterSpacing: 0,
-                    color: "#282828" }}>{Strings.setting.language}</Text>
-
-          <View style={{ backgroundColor: 'white' }}>
-            <LanguageItem value={1} currentValue={languageValue} text="ENGLISH" icon={flag_en} onValueChange={this.onLanguageValueChange} />
-            <LanguageItem value={0} currentValue={languageValue} text="TIẾNG VIỆT" icon={flag_vn} onValueChange={this.onLanguageValueChange} />
+        <ScrollView style={{ borderTopRightRadius: 20, marginVertical: 10 }}>
+          <View style={{ backgroundColor: "white" }}>
+            <LanguageItem
+              value={1}
+              currentValue={languageValue}
+              text="ENGLISH"
+              icon={flag_en}
+              onValueChange={this.onLanguageValueChange}
+            />
+            <LanguageItem
+              value={0}
+              currentValue={languageValue}
+              text="TIẾNG VIỆT"
+              icon={flag_vn}
+              onValueChange={this.onLanguageValueChange}
+            />
           </View>
         </ScrollView>
       </View>
-
     );
   }
 
   onLanguageValueChange = (value) => {
-    this.props.resetStateByKey({ language: value == 0 ? 'vi' : 'en' })
-  }
+    this.props.resetStateByKey({ language: value == 0 ? "vi" : "en" });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   menuContent: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingVertical: 10
-  }
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+  },
 });
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   user: state.auth.user,
   enableVibrate: state.app.enableVibrate,
   enableSound: state.app.enableSound,
   enableNotification: state.app.enableNotification,
   language: state.app.language,
-  isLoading: state.auth.isLoading
+  isLoading: state.auth.isLoading,
 });
 const mapActionToState = {
-  resetStateByKey
+  resetStateByKey,
 };
 
-export default connect(mapStateToProps, mapActionToState)(SettingScreen);
+export default connect(
+  mapStateToProps,
+  mapActionToState
+)(SettingScreen);
