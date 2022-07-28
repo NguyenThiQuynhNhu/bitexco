3a31fde12fd185a9f8dcf268d789c0ffda5ab27a
Ho Anh Nam <85169417+namanh1909@users.noreply.github.com>
2022-07-28 09:13:42 +0700

    fix popup

--- src/resident/screens/utility/services/ServiceBasic/CommentView.js
+++ src/resident/screens/utility/services/ServiceBasic/CommentView.js
@@ -14,7 +14,7 @@ import LinearGradient from "react-native-linear-gradient";
 import { MyIcon } from "../../../../theme/icons";
 import colors from "../../../../theme/colors";
 import Strings from "../../../../utils/languages";
-
+import responsive from "../../../../../resources/responsive";
 // create a component
 const CommentView = ({ onYes, onChangeText, onClose }) => {
   return (
@@ -22,110 +22,122 @@ const CommentView = ({ onYes, onChangeText, onClose }) => {
     //   source={require("../../../../resources/popupBg.png")}
     //   style={{ height: 250 }}
     // >
-    <View style={styles.linearGradient}>
-      <View
-        style={{
-          borderRadius: 16,
-        }}
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#fff", "#cecece"]}
+        style={styles.linearGradient}
       >
         <View
           style={{
-            padding: 10,
-            justifyContent: "center",
-            alignItems: "center",
+            borderRadius: 16,
           }}
         >
-          <Text
+          <View
             style={{
-              color: "white",
-              fontFamily: "Inter-SemiBold",
-              fontSize: 18,
-              fontWeight: "600",
-              fontStyle: "normal",
-              letterSpacing: 0,
-              textAlign: "center",
-              textTransform: "uppercase",
-            }}
-          >
-            {Strings.app.confirm}
-          </Text>
-        </View>
-        <View>
-          <TextInput
-            autoFocus
-            autoCorrect={false}
-            style={{
-              backgroundColor: "#fff",
-              height: 100,
-              borderRadius: 8,
-              borderWidth: 1,
-              margin: 10,
-              marginLeft: 10,
-              borderColor: colors.grayBorder,
-              textAlignVertical: "top",
-              fontFamily: "Inter-Regular",
-              fontSize: 14,
-            }}
-            onSubmitEditing={onYes}
-            underlineColorAndroid="transparent"
-            multiline={true}
-            placeholder={Strings.app.description}
-            onChangeText={onChangeText}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            marginTop: 20,
-            alignSelf: "center",
-            marginBottom: 10,
-          }}
-        >
-          <TouchableOpacity
-            onPress={onClose}
-            style={{
-              height: 50,
-              width: 50,
-              borderRadius: 25,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "#ffffff",
-              borderStyle: "solid",
-              borderWidth: 1,
-              borderColor: colors.appTheme,
-            }}
-          >
-            <MyIcon name="x" size={30} color={colors.appTheme} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              marginLeft: 50,
-              height: 50,
-              width: 50,
-              borderRadius: 25,
+              padding: 10,
               justifyContent: "center",
               alignItems: "center",
               backgroundColor: colors.appTheme,
+              borderTopRightRadius: 16,
+              borderTopLeftRadius: 16,
+              height: responsive.h(55),
             }}
-            onPress={onYes}
           >
-            <MyIcon name="check" size={30} color="#fff" />
-          </TouchableOpacity>
+            <Text
+              style={{
+                color: "white",
+                fontFamily: "Inter-SemiBold",
+                fontSize: 16,
+                fontWeight: "600",
+                fontStyle: "normal",
+                letterSpacing: 0,
+                textAlign: "center",
+                textTransform: "uppercase",
+              }}
+            >
+              {Strings.app.confirm}
+            </Text>
+          </View>
+
+          <View>
+            <TextInput
+              autoFocus
+              autoCorrect={false}
+              style={{
+                backgroundColor: "#fff",
+                height: 100,
+                borderRadius: 8,
+                borderWidth: 1,
+                padding: 10,
+                margin: 20,
+                borderColor: colors.grayBorder,
+                textAlignVertical: "top",
+                fontFamily: "Inter-Regular",
+                fontSize: 14,
+              }}
+              onSubmitEditing={onYes}
+              underlineColorAndroid="transparent"
+              multiline={true}
+              placeholder={Strings.app.description}
+              onChangeText={onChangeText}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 20,
+              alignSelf: "center",
+              marginBottom: 10,
+            }}
+          >
+            <TouchableOpacity
+              onPress={onClose}
+              style={{
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#ffffff",
+                borderStyle: "solid",
+                borderWidth: 1,
+                borderColor: colors.gray1,
+              }}
+            >
+              <MyIcon name="x" size={30} color={colors.gray1} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                marginLeft: 50,
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: colors.appTheme,
+              }}
+              onPress={onYes}
+            >
+              <MyIcon name="check" size={30} color="#fff" />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </LinearGradient>
     </View>
-    // </ImageBackground>
   );
 };
 
 // define your styles
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
+    width: "90%",
+    height: responsive.h(250),
+    borderRadius: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: "#b1b1b1",
   },
   linearGradient: {
-    width: "90%",
     borderRadius: 16,
   },
 });
--- src/resident/screens/utility/services/ServiceExtension/CommentView.js
+++ src/resident/screens/utility/services/ServiceExtension/CommentView.js
@@ -1,119 +1,140 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, KeyboardAvoidingView, TextInput, Platform, TouchableOpacity } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import { MyIcon } from '../../../../theme/icons';
-import colors from '../../../../theme/colors';
-import Strings from '../../../../utils/languages';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import { MyIcon } from "../../../../theme/icons";
+import colors from "../../../../theme/colors";
+import Strings from "../../../../utils/languages";
+import responsive from "../../../../../resources/responsive";
 
 // create a component
 const CommentView = ({ onYes, onChangeText, onClose }) => {
-    return (
-
-
-        <LinearGradient
-            colors={[colors.appTheme, '#fff']}
-            style={styles.linearGradient}
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#fff", "#cecece"]}
+        style={styles.linearGradient}
+      >
+        <View
+          style={{
+            borderRadius: 16,
+          }}
         >
-            <View
-                style={{
-                    borderRadius: 16
-                }}
+          <View
+            style={{
+              padding: 10,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: colors.appTheme,
+              borderTopRightRadius: 16,
+              borderTopLeftRadius: 16,
+              height: responsive.h(55),
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontFamily: "Inter-SemiBold",
+                fontSize: 16,
+                fontWeight: "600",
+                fontStyle: "normal",
+                letterSpacing: 0,
+                textAlign: "center",
+                textTransform: "uppercase",
+              }}
             >
-                <View style={{ padding: 10, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{
-                        color: 'white',
-                        fontFamily: "Inter-SemiBold",
-                        fontSize: 18,
-                        fontWeight: "600",
-                        fontStyle: "normal",
-                        letterSpacing: 0,
-                        textAlign: "center"
-                    }}>{Strings.app.confirm}</Text>
-                </View>
-                <View>
-                    <TextInput
-                        autoFocus
-                        autoCorrect={false}
-                        style={{
-                            backgroundColor: '#fff',
-                            height: 100,
-                            borderRadius: 8,
-                            borderWidth: 1,
-                            margin: 10,
-                            marginLeft: 10,
-                            borderColor: colors.grayBorder,
-                            textAlignVertical: 'top',
-                            fontFamily: "Inter-Regular",
-                            fontSize: 14,
-                        }}
-
-                        onSubmitEditing={onYes}
-                        underlineColorAndroid="transparent"
-                        multiline={true}
-                        placeholder={Strings.app.description}
-                        onChangeText={onChangeText}
-                    />
-                </View>
-                <View style={{ flexDirection: 'row', marginTop: 20, alignSelf: 'center', marginBottom: 10 }}>
-
-                    <TouchableOpacity
-                        onPress={onClose}
-                        style={{
-                            height: 50,
-                            width: 50,
-                            borderRadius: 25,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            backgroundColor: "#ffffff",
-                            borderStyle: "solid",
-                            borderWidth: 1,
-                            borderColor: colors.appTheme
-                        }}
-                    >
-                        <MyIcon
-                            name="x"
-                            size={30}
-                            color={colors.appTheme}
-                        />
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={{
-                            marginLeft: 50,
-                            height: 50,
-                            width: 50,
-                            borderRadius: 25,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            backgroundColor: colors.appTheme
-                        }}
-                        onPress={onYes}
-                    >
-                        <MyIcon
-                            name="check"
-                            size={30}
-                            color="#fff"
-                        />
-                    </TouchableOpacity>
-                </View>
-
-            </View>
-        </LinearGradient>
-
-    );
-}
+              {Strings.app.confirm}
+            </Text>
+          </View>
+          <View>
+            <TextInput
+              autoFocus
+              autoCorrect={false}
+              style={{
+                backgroundColor: "#fff",
+                height: 100,
+                borderRadius: 8,
+                borderWidth: 1,
+                padding: 10,
+                margin: 20,
+                borderColor: colors.grayBorder,
+                textAlignVertical: "top",
+                fontFamily: "Inter-Regular",
+                fontSize: 14,
+              }}
+              onSubmitEditing={onYes}
+              underlineColorAndroid="transparent"
+              multiline={true}
+              placeholder={Strings.app.description}
+              onChangeText={onChangeText}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 20,
+              alignSelf: "center",
+              marginBottom: 10,
+            }}
+          >
+            <TouchableOpacity
+              onPress={onClose}
+              style={{
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#ffffff",
+                borderStyle: "solid",
+                borderWidth: 1,
+                borderColor: colors.gray1,
+              }}
+            >
+              <MyIcon name="x" size={30} color={colors.gray1} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                marginLeft: 50,
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: colors.appTheme,
+              }}
+              onPress={onYes}
+            >
+              <MyIcon name="check" size={30} color="#fff" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </LinearGradient>
+    </View>
+  );
+};
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    linearGradient: {
-        width: '90%',
-        borderRadius: 16,
-        backgroundColor: colors.appTheme
-    },
+  container: {
+    backgroundColor: "#fff",
+    width: "90%",
+    height: responsive.h(250),
+    borderRadius: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: "#b1b1b1",
+  },
+  linearGradient: {
+    borderRadius: 16,
+  },
 });
 
-export default (CommentView);
+export default CommentView;
