34ce3e6700ee1076c9f57896c123eeb259e496a8
Ho Anh Nam <85169417+namanh1909@users.noreply.github.com>
2022-08-01 10:57:33 +0700

    ca truc + checklist

--- src/components/Checklist/List/ButtonFilter.js
+++ src/components/Checklist/List/ButtonFilter.js
@@ -1,65 +1,69 @@
 //import liraries
-import React, { Component } from 'react';
-import { Screen } from '../../../utils/device'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import fontSize from '../../../theme/fontsize';
-import {  converStatusToString } from '../../../utils/checklist';
-import colors from '../../../theme/colors';
+import React, { Component } from "react";
+import { Screen } from "../../../utils/device";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import fontSize from "../../../theme/fontsize";
+import { converStatusToString } from "../../../utils/checklist";
+import colors from "../../../theme/colors";
 
-import fontsize from '../../../theme/fontsize';
+import fontsize from "../../../theme/fontsize";
+import responsive from "../../../resources/responsive";
 
-const Devices = require('react-native-device-detection');
+const Devices = require("react-native-device-detection");
 
 // create a component
 const ButtonFilter = (props) => {
-    const { onSelectedChange, value, currentValue, textColor, style } = props
-    const text = converStatusToString(value)
+  const { onSelectedChange, value, currentValue, textColor, style } = props;
+  const text = converStatusToString(value);
 
-    return (
-        <TouchableOpacity
-
-            onPress={() => onSelectedChange(value)}
+  return (
+    <TouchableOpacity onPress={() => onSelectedChange(value)}>
+      <View
+        style={{
+          alignItems: "center",
+          marginTop: 10,
+          flexDirection: "column",
+          justifyContent: "center",
+          ...style,
+        }}
+      >
+        <Text
+          numberOfLines={1}
+          style={{
+            fontFamily: value !== currentValue ? "Inter-Regular" : "Inter-Bold",
+            fontSize: responsive.h(15),
+            fontWeight: value !== currentValue ? "normal" : "bold",
+            fontStyle: "normal",
+            letterSpacing: 0,
+            textAlign: "center",
+            color: value !== currentValue ? "#c8c8c8" : "#3d3d3d",
+            paddingHorizontal: 10,
+          }}
         >
-            <View
-                style={{
-                    alignItems: 'center',
-                    width: Devices.isTablet ? (Screen.width / 5) : (Screen.width / 4),
-                    marginTop: 10,
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    ...style
-                    
-                }}>
-                <Text style={{
-                    fontFamily: value !== currentValue ? "Inter-Regular" : "Inter-Bold",
-                    fontSize: 13,
-                    fontWeight: value !== currentValue ? "normal" : "bold",
-                    fontStyle: "normal",
-                    letterSpacing: 0,
-                    textAlign: "center",
-                    color: value !== currentValue ? '#c8c8c8' : "#3d3d3d"
-                }}>{text}</Text>
-                <View style={{
-                    width: Devices.isTablet ? 64 : 44,
-                    height: 3,
-                    borderRadius: 4,
-                    backgroundColor: value !== currentValue ? '#fff' : '#a3cd80',
-                    marginTop: 2.5
-                }}></View>
-            </View>
-            
-        </TouchableOpacity>
-    );
+          {text}
+        </Text>
+        <View
+          style={{
+            width: Devices.isTablet ? 64 : 44,
+            height: 3,
+            borderRadius: 4,
+            backgroundColor: value !== currentValue ? "#fff" : "#a3cd80",
+            marginTop: 2.5,
+          }}
+        />
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2c3e50",
+  },
 });
 
 //make this component available to the app
